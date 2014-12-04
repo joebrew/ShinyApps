c807f854ebd8e0271324ec3335b70bac30b9f95e
Joe Brew <joebrew@gmail.com>
2014-12-04 11:28:53 -0500

    expansion of spanish app

--- spanish/expand.R
+++ spanish/expand.R
@@ -12,3 +12,24 @@ x <- dat %>%
 subject <- rep(dat$subject[1:10], each = 100)
 verb <- rep(dat$verb, 100)
 form <- rep(dat$form[1:8])
+
+subject <- dat$subject[1:10]
+verb <- dat$verb
+form <- dat$form[1:8]
+
+z <- vector(length = 1, mode = "character")
+
+for (i in 1:length(subject)){
+  for (j in 1:length(verb)){
+    for (k in 1:length(form)){
+      x <- paste(subject[i],
+            verb[j],
+            form[k], sep = "+")
+      z <- c(z, x)
+    }
+  }
+}
+
+# Split string on spaces
+z <- data.frame(matrix(unlist(strsplit(z, split = "[+]")), ncol = 3, byrow = T))
+names(z) <- c("subject", "verb", "form")
