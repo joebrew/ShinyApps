d4f50d5d6267a533e9aeb7b4ecd544a1a4508b55
Joe Brew <joebrew@gmail.com>
2015-01-10 13:48:19 -0500

    minimal report generator for eric

---
+++ minimal/server.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(knitr)
+
+# Define server
+shinyServer(function(input, output) {
+
+  # Define downloadHandler object
+  output$downloadPDF <-
+    downloadHandler(filename = "report.pdf",
+                    content = function(file){
+                      # generate PDF
+                      knit2pdf("report.Rnw")
+                      
+                      # copy pdf to 'file'
+                      file.copy("report.pdf", file)
+                      
+                      # delete generated files
+                      file.remove("report.pdf", "report.tex",
+                                  "report.aux", "report.log")
+                      
+                      # delete folder with plots
+                      unlink("figure", recursive = TRUE)
+                    },
+                    contentType = "application/pdf"
+    )
+  
+  
+  
+})
---
+++ minimal/ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(knitr)
+
+# Define UI for application
+shinyUI(pageWithSidebar(
+  
+  ## Application title
+  headerPanel("Eric's favorite color, number and fish!"),
+  
+  ## Sidebar panel
+  sidebarPanel("This app generates a pdf report about some of Eric's favorite things.  
+               The contents of this report are controlled by user input.",
+               br(),
+               
+               "The source code is available on ",
+               a(href = "https://github.com/joebrew/ShinyApps",
+                 "GitHub."),
+               downloadButton("downloadPDF", "Download your report, you asshole!")
+  ),
+  
+  
+  ## Main panel
+  mainPanel(
+    wellPanel(
+      textInput("color",
+                "Eric's favorite color:",
+                "girly pink"),
+      selectInput("fish", "Eric's favorite fish",
+                  c("Goldfish" = "goldfish",
+                    "Whale" = "whale",
+                    "Catfish" = "catfish",
+                    "Phish" = "phish")),
+      sliderInput("number", "Eric's favorite number",
+                  min=1, max=10, value=6, step=1)
+  )
+)
+))
+
